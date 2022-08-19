5e23c42217f4fc56813010198c2dcebf30e242b7
lencamo <207141481@qq.com>
2022-08-19 13:34:47 +0800

    前端vue项目前期准备

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    // port: 8080  // 默认为8080
+    open: true // 启动项目时，自动打开浏览器
+  }
 })
