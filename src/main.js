ac26738176826aef8dafd34282b7d08dbee8c773
lencamo <207141481@qq.com>
2022-08-20 09:48:44 +0800

    axios和echarts全局引入

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+
+// 全局配置axios的请求🌈根路径
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
+Vue.prototype.$http = axios
+
+// 将echarts对象全局挂载到vue原型对象上
+Vue.prototype.$echarts = window.echarts
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
